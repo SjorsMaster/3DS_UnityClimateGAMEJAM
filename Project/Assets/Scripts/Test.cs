be0583e9e0084d18a06ca1b9f188b27929e6fcd1
Sjors Kaagman <sjorspnk@gmail.com>
2019-02-11 11:20:24 +0100

    Initial Commit

---
+++ Project/Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    
+#if UNITY_N3DS
+    // Nintendo 3DS-specific code here
+#endif
+
+
+
+
+}
